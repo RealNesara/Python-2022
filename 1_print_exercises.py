e7211c8367562c53467ce7a1477964c4fd8fe148
RealNesara <neswew13@kingshigh.school.nz>
2022-02-22 12:15:01 +1300

    Did first exercise

---
+++ 1_print_exercises.py
@@ -0,0 +1,22 @@
+#Print name
+print("Nesara Wewalage")
+#Teacher name
+print("Luke Smith")
+#School name
+print("Kings High School")
+#Age
+print("13")
+#Hours in favourite game
+print("100")
+#Year in 25 years
+print(2022+25)
+#How old I'll be in 100 years 
+print(13+100)
+#Amswer 658/7
+print(658/7)
+#How many years ago moon landing
+print("2022-1969")
+#Favourite game
+print("GTA5")
+#Favourite class
+print("Science")
