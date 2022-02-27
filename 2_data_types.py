87fcb1d4b3f1230e2cbe209d9d255d5eaccf37e2
RealNesara <neswew13@kingshigh.school.nz>
2022-02-28 09:55:41 +1300

    3_variables.py

---
+++ 2_data_types.py
@@ -0,0 +1,62 @@
+#1
+print ("Baby shark, doo doo doo doo doo doo\n Baby shark, doo doo doo doo doo doo\n Baby shark, doo doo doo doo doo doo\n Baby shark!")
+#2
+print("\"I'm the king of the world\"-Titanic")
+#3
+print("String is the name of something, e.g. \"An item")
+#4
+print("Integers are whole numbers e.g. \"4,100,-4")
+#5
+print("Floats are numbers with decimal points")
+#6
+print(543/3)
+#7
+print(543//3)
+#8 
+print(543%3)
+#9
+print(1000%9)
+#10
+print(434//10)
+#11
+print(3%2)
+#12
+print(5%5)
+#13
+print(555/2)
+#14
+print(123/2)
+#15
+print(123//2)
+#16
+print(123%2)
+#17
+print(764%2)
+#18
+print(165//2)
+#19
+print(4.3+2.1)
+#20
+print(100+42)
+#21
+print(4.5-4)
+#22
+print(5-3.2)
+#23
+print(20/4)
+#24
+print(6*2.1)
+#25
+print(5*5)
+#26
+print("Nesara"*10)
+#27
+print("Nesara\n"*10)
+#28
+print("5+10="+ str(5+10))
+#29
+print("15/2="+ str(15/2))
+#30
+print("30/2*5="+ str(30/2*5))
+#31
+print("30/2*5="+ str(30/2*5))
