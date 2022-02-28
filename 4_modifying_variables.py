538fc507f19974a6012043301a3022665857173c
RealNesara <neswew13@kingshigh.school.nz>
2022-03-01 10:42:29 +1300

    Create 4_modifying_variables.py

---
+++ 4_modifying_variables.py
@@ -0,0 +1,39 @@
+#1
+money=200
+hat=20
+top=30
+pants=15
+belt=60
+shoes=40
+#2
+print("player has $"+str(money)+ "In their bag")
+#3
+print("Buying a hat")
+#4
+money=money-hat
+#5
+print("player now has $"+str(money)+ "In their bag")
+#6
+money=200
+#7
+#Hat
+print(money)
+print("Buying a hat")
+money=money-hat
+#Top
+print(money)
+print("Buying a top")
+money=money-top
+#Pants
+print(money)
+print("Buying pants")
+money=money-pants
+#Belt
+print(money)
+print("Buying a belt")
+money=money-belt
+#Shoes
+print(money)
+print("Buying shoes")
+money=money-shoes
+print("player has $"+str(money)+ "In your bag")
