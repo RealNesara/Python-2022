7bd74ac402f4b5ca0d5d96cbc0fd5c83d8e5e6a2
RealNesara <neswew13@kingshigh.school.nz>
2022-03-04 14:26:31 +1300

    Create 5_input.py

---
+++ 5_input.py
@@ -0,0 +1,20 @@
+#1
+user_push_button= input("Press Enter.\n")
+#2
+user_name= input("Enter your full name and press enter\n")
+#3
+user_age= input("Enter your age and press enter.\n")
+#6
+user_favourite_movie= input("Enter favourite movie.\n")
+#7
+user_name_book= input("Enter book name.\n")
+#8
+user_adjective= input("Enter adjective.\n")
+#9
+user_noun= input("Enter noun.\n")
+#10 
+user_verb= input("Enter verb.\n")
+#11
+print (f"Welcome, {user_name}, you are {user_age} years old and you like {user_favourite_movie}")
+print (f"{user_favourite_movie} is a really good film, I really like it when the {user_adjective} {user_noun}'s are {user_verb}")
+print (f"The book {user_name_book} is a interesting chooice, one I personaly wound't read")
